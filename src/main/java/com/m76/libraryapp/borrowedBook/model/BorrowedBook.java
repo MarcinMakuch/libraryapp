44aeb3029a693158a316fbbcedb5bc5b57455f16
unknown <makuchmarcin0@gmail.com>
2022-05-03 12:06:13 +0200

    borrowedBook class added

---
+++ src/main/java/com/m76/libraryapp/borrowedBook/model/BorrowedBook.java
@@ -0,0 +1,26 @@
+package com.m76.libraryapp.borrowedBook.model;
+
+
+import com.m76.libraryapp.book.model.Book;
+import com.m76.libraryapp.user.model.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "borrowedbooks")
+public class BorrowedBook {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private User user;
+
+    @OneToOne
+    private Book book;
+
+}
